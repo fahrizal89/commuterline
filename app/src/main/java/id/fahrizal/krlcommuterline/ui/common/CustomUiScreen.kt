b73ad6681922e66775ff17605d06fedb484b66ba
sentosaf <fahrizal.sentosa@dana.id>
2023-10-22 13:05:37 +0700

    rename file and remove unused package of Text

--- app/src/main/java/id/fahrizal/krlcommuterline/ui/common/CustomUiScreen.kt
+++ app/src/main/java/id/fahrizal/krlcommuterline/ui/common/CustomUiScreen.kt
@@ -16,11 +16,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import id.fahrizal.krlcommuterline.R
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.delay
@@ -103,7 +105,7 @@ fun DebouncedEditText(
 @Composable
 fun ClickableText(label:String, text:String="", onClick: () -> Unit){
     Column {
-        androidx.compose.material3.Text(
+        Text(
             text = label,
             modifier = Modifier.padding(8.dp, 0.dp, 8.dp, 0.dp),
             fontSize = MaterialTheme.typography.caption.fontSize,
@@ -115,11 +117,12 @@ fun ClickableText(label:String, text:String="", onClick: () -> Unit){
                 .padding(8.dp, 0.dp, 8.dp, 8.dp)
                 .clickable { onClick() }
         ) {
-            androidx.compose.material3.Text(
+            Text(
                 text = text,
                 modifier = Modifier
                     .padding(8.dp, 8.dp, 8.dp, 8.dp)
-                    .fillMaxWidth()
+                    .fillMaxWidth(),
+                color = colorResource(id = R.color.teal_700),
             )
             Divider(modifier = Modifier.padding(8.dp, 0.dp, 8.dp, 4.dp))
         }
