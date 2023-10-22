9acbd2054461d8d72d931a93fbedba5495b1fbdc
sentosaf <fahrizal.sentosa@dana.id>
2023-10-22 09:13:24 +0700

    GuideItemStraight using shape

--- app/src/main/java/id/fahrizal/krlcommuterline/ui/guide/GuideItemStraight.kt
+++ app/src/main/java/id/fahrizal/krlcommuterline/ui/guide/GuideItemStraight.kt
@@ -1,6 +1,5 @@
 package id.fahrizal.krlcommuterline.ui.guide
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -10,11 +9,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import id.fahrizal.krlcommuterline.R
+import id.fahrizal.krlcommuterline.ui.common.DotCircle
+import id.fahrizal.krlcommuterline.ui.common.VerticalLine
 
 @Composable
 fun GuideItemStraight(
@@ -27,17 +26,14 @@ fun GuideItemStraight(
         Box(
             modifier = Modifier.width(52.dp),
         ) {
-            Line(
-                modifier = Modifier.width(44.dp)
-                    .padding(start = 16.dp),
-                lineColorInt = colorLineInt,
-                heightInt = 48
+            VerticalLine(
+                modifier = Modifier.padding(start = 29.dp),
+                height = 32.dp,
+                color = colorResource(id = colorLineInt)
             )
-            Image(
-                modifier = Modifier.width(44.dp).padding(start = 22.dp, end = 6.dp, top = 14.dp),
-                painter = painterResource(R.drawable.baseline_circle_24),
-                contentDescription = "img_circle",
-                colorFilter = ColorFilter.tint(colorResource(id = colorDotInt))
+            DotCircle(
+                modifier = Modifier.padding(start = 20.dp, top = 10.dp),
+                color = colorResource(id = colorDotInt)
             )
 
         }
