4e258198f898ab510cc6817785ef633b83f6559d
sentosaf <fahrizal.sentosa@dana.id>
2023-10-29 21:33:08 +0700

    inquiry data station detail by id

--- app/src/main/java/id/fahrizal/krlcommuterline/ui/station/detail/StationDetailViewModel.kt
+++ app/src/main/java/id/fahrizal/krlcommuterline/ui/station/detail/StationDetailViewModel.kt
@@ -1,22 +1,38 @@
 package id.fahrizal.krlcommuterline.ui.station.detail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import id.fahrizal.krlcommuterline.data.model.Station
+import id.fahrizal.krlcommuterline.domain.usecase.FindStation
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class StationDetailViewModel @Inject constructor(
-
+    private val ioCoroutineDispatcher: CoroutineDispatcher,
+    private val findStation: FindStation
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow<StationDetailUiState>(StationDetailUiState.Loading)
     val uiState: StateFlow<StationDetailUiState> = _uiState
 
     fun fetchStation(id:Int) {
-        id.toString()
+        viewModelScope.launch(ioCoroutineDispatcher) {
+            findStation(id)
+                .catch {
+                    Timber.e(it)
+                    _uiState.value = StationDetailUiState.Error(it.toString())
+                }
+                .collect {
+                    _uiState.value = StationDetailUiState.Loaded(it)
+                }
+        }
 
         _uiState.value = StationDetailUiState.Loaded()
     }
