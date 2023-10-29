734ff4f5782718d885768bffb282c82dc4d0c9b5
sentosaf <fahrizal.sentosa@dana.id>
2023-10-29 20:26:08 +0700

    adjust api all-stations with new package

--- app/src/main/java/id/fahrizal/krlcommuterline/data/repository/route/source/remote/RouteApi.kt
+++ app/src/main/java/id/fahrizal/krlcommuterline/data/repository/route/source/remote/RouteApi.kt
@@ -4,6 +4,6 @@ import id.fahrizal.krlcommuterline.data.model.Route
 import retrofit2.http.GET
 
 interface RouteApi {
-    @GET("commuterline/main/app/api/all-stations.json")
+    @GET("commuterline/main/be/all-stations.json")
     suspend fun getRoute(): Route
 }
