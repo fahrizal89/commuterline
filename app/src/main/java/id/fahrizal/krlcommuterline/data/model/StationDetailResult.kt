98a12e84e083fe9edb30961b363e8aa2c3120602
sentosaf <fahrizal.sentosa@dana.id>
2023-10-31 15:49:30 +0700

    remove unused annotation Entity and adjust format

--- app/src/main/java/id/fahrizal/krlcommuterline/data/model/StationDetailResult.kt
+++ app/src/main/java/id/fahrizal/krlcommuterline/data/model/StationDetailResult.kt
@@ -1,11 +1,8 @@
 package id.fahrizal.krlcommuterline.data.model
 
-import androidx.room.Entity
-
-@Entity(tableName = "station")
-class StationDetailResult (
-    var id:Int?=null,
-    var distance:Int = 1, //distance
-    var next:Int = -1,
-    var codes:List<String> = ArrayList()
+class StationDetailResult(
+    var id: Int? = null,
+    var distance: Int = 1, // distance
+    var next: Int = -1,
+    var codes: List<String> = ArrayList(),
 )
