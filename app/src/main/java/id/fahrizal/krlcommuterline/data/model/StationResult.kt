6c5d0de16af414e03e5a12afc20c40ddcccd1ab7
sentosaf <fahrizal.sentosa@dana.id>
2023-10-31 15:34:46 +0700

    change position url

--- app/src/main/java/id/fahrizal/krlcommuterline/data/model/StationResult.kt
+++ app/src/main/java/id/fahrizal/krlcommuterline/data/model/StationResult.kt
@@ -4,6 +4,6 @@ package id.fahrizal.krlcommuterline.data.model
 data class StationResult (
     var id:Int?=null,
     var name:String?="",
-    var url:String?="",
     var branch: HashMap<String, StationDetailResult>?=HashMap(),
+    var url:String?="",
 )
